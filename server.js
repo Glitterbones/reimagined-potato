434062877fed3323540b1956ab1bdd1af5546b6c
ashley lockhart <allockhart49@gmail.com>
2022-04-20 20:55:48 -0600

    routes

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+
+const { notes } = require('./db/db.json');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
